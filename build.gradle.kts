21a4ecd9506336df91bdeb50960b82e494d35f0e
Uiniel <174327181+Uiniel@users.noreply.github.com>
2025-07-13 22:52:59 +0200

    Conversion to native addon

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "8.3.7"
+}
+
+group = "me.owies"
+version = "0.3.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven("https://repo.bluecolored.de/releases")
+    maven("https://repo.bluecolored.de/snapshots")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("com.technicjelle:BMUtils:4.3.1")
+
+    compileOnly("de.bluecolored:bluemap-core:5.9-feat.atlases-13")
+    compileOnly("de.bluecolored:bluemap-common:5.7")
+    compileOnly("de.bluecolored:bluemap-api:2.7.4")
+    compileOnly("me.owies:bluemapmodelloaders-common:0.2.2")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+
+tasks.shadowJar {
+    relocate("com.technicjelle.BMUtils", "${project.group}.${project.name}.BMUtils")
+}
+
+tasks.processResources {
+    filesMatching("bluemap.addon.json") {
+        expand(project.properties)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/me/owies/bluemapmodelloaders/resources/ModelLoaderResourcePack.java
@@ -0,0 +1,168 @@
+package me.owies.bluemapmodelloaders.resources;
+
+import de.bluecolored.bluemap.core.BlueMap;
+import de.bluecolored.bluemap.core.resources.ResourcePath;
+import de.bluecolored.bluemap.core.resources.adapter.ResourcesGson;
+import de.bluecolored.bluemap.core.resources.pack.Pack;
+import de.bluecolored.bluemap.core.resources.pack.ResourcePool;
+import de.bluecolored.bluemap.core.resources.pack.resourcepack.ResourcePack;
+import de.bluecolored.bluemap.core.resources.pack.resourcepack.ResourcePackExtension;
+import de.bluecolored.bluemap.core.resources.pack.resourcepack.blockstate.VariantSet;
+import de.bluecolored.bluemap.core.resources.pack.resourcepack.texture.Texture;
+import de.bluecolored.bluemap.core.util.Key;
+import lombok.Getter;
+import me.owies.bluemapmodelloaders.Constants;
+import me.owies.bluemapmodelloaders.resources.obj.ObjMaterialLibrary;
+import me.owies.bluemapmodelloaders.resources.obj.ObjModel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+public class ModelLoaderResourcePack implements ResourcePackExtension {
+    private ResourcePack blueMapResourcePack;
+
+    @Getter
+    private final ResourcePool<ExtendedModel> models;
+    @Getter
+    private final ResourcePool<ObjModel> objModels;
+    @Getter
+    private final ResourcePool<ObjMaterialLibrary> mtlLibraries;
+
+    public ModelLoaderResourcePack(ResourcePack blueMapResourcePack) {
+        this.models = new ResourcePool<>();
+        this.objModels = new ResourcePool<>();
+        this.mtlLibraries = new ResourcePool<>();
+        this.blueMapResourcePack = blueMapResourcePack;
+    }
+
+    @Override
+    public void loadResources(Iterable<Path> roots) throws IOException, InterruptedException {
+        for (Path root : roots) {
+            blueMapResourcePack.loadResourcePath(root, this::loadResourcesFromPath);
+        }
+    }
+
+    public void loadResourcesFromPath(Path root) throws IOException {
+
+        try {
+            CompletableFuture.allOf(
+                    // load model extensions
+                    CompletableFuture.runAsync(() -> {
+                        ResourcePack.list(root.resolve("assets"))
+                                .map(path -> path.resolve("models"))
+                                .flatMap(ResourcePack::list)
+                                .filter(path -> !path.getFileName().toString().equals("item"))
+                                .flatMap(ResourcePack::walk)
+                                .filter(path -> path.getFileName().toString().endsWith(".json"))
+                                .filter(Files::isRegularFile)
+                                .forEach(file -> models.load(
+                                        new ResourcePath<>(root.relativize(file), 1, 3),
+                                        key -> {
+                                            try (BufferedReader reader = Files.newBufferedReader(file)) {
+                                                return ResourcesGson.INSTANCE.fromJson(reader, ExtendedModel.class);
+                                            }
+                                        }
+                                ));
+                    }, BlueMap.THREAD_POOL),
+
+                    // load .obj models
+                    CompletableFuture.runAsync(() -> {
+                        ResourcePack.list(root.resolve("assets"))
+                                .map(path -> path.resolve("models"))
+                                .flatMap(ResourcePack::list)
+                                .filter(path -> !path.getFileName().toString().equals("item"))
+                                .flatMap(ResourcePack::walk)
+                                .filter(path -> path.getFileName().toString().endsWith(".obj"))
+                                .filter(Files::isRegularFile)
+                                .forEach(file -> objModels.load(
+                                        new ResourcePath<>(root.relativize(file.resolveSibling(file.getFileName() + ".")), 1, 2),
+                                        key -> {
+                                            try (BufferedReader reader = Files.newBufferedReader(file)) {
+                                                return ObjModel.fromReader(reader, root.relativize(file), 1, 2);
+                                            }
+                                        }
+                                ));
+                    }, BlueMap.THREAD_POOL),
+
+                    // load .mtl models
+                    CompletableFuture.runAsync(() -> {
+                        ResourcePack.list(root.resolve("assets"))
+                                .map(path -> path.resolve("models"))
+                                .flatMap(ResourcePack::list)
+                                .filter(path -> !path.getFileName().toString().equals("item"))
+                                .flatMap(ResourcePack::walk)
+                                .filter(path -> path.getFileName().toString().endsWith(".mtl"))
+                                .filter(Files::isRegularFile)
+                                .forEach(file -> mtlLibraries.load(
+                                        new ResourcePath<>(root.relativize(file.resolveSibling(file.getFileName() + ".")), 1, 2),
+                                        key -> {
+                                            try (BufferedReader reader = Files.newBufferedReader(file)) {
+                                                return ObjMaterialLibrary.fromReader(reader);
+                                            }
+                                        }
+                                ));
+                    }, BlueMap.THREAD_POOL)
+            ).join();
+        } catch (RuntimeException ex) {
+            Throwable cause = ex.getCause();
+            if (cause instanceof IOException) throw (IOException) cause;
+            if (cause != null) throw new IOException(cause);
+            throw new IOException(ex);
+        }
+    }
+
+    @Override
+    public Set<Key> collectUsedTextureKeys() {
+        HashSet<Key> keys = new HashSet<>();
+        keys.add(new ResourcePath<Texture>("bluemapmodelloaders:block/white"));
+        models
+                .values()
+                .stream()
+                .flatMap(ExtendedModel::getUsedTextures)
+                .forEach(keys::add);
+
+        return keys;
+    }
+
+    @Override
+    public void bake() throws IOException {
+        models.values().forEach(model -> model.applyParent(this));
+        models.values().forEach(model -> model.bake(blueMapResourcePack, this));
+
+        blueMapResourcePack
+                .getBlockStates()
+                .values()
+                .stream()
+                .flatMap(blockState -> {
+                    Stream<VariantSet> variants = Stream.empty();
+                    if (blockState.getVariants() != null) {
+                        variants = Arrays.stream(blockState.getVariants().getVariants());
+                        VariantSet defaultVariant = blockState.getVariants().getDefaultVariant();
+                        if (defaultVariant != null) {
+                            variants = Stream.concat(variants, Stream.of(blockState.getVariants().getDefaultVariant()));
+                        }
+                    }
+                    Stream<VariantSet> multipart = Stream.empty();
+                    if (blockState.getMultipart() != null) {
+                        multipart = Arrays.stream(blockState.getMultipart().getParts());
+                    }
+                    return Stream.concat(variants, multipart);
+                })
+                .flatMap(v -> Arrays.stream(v.getVariants()))
+                .forEach(variant -> {
+                    ExtendedModel model = models.get(variant.getModel());
+                    if (model == null) return;
+
+                    LoaderType<?> loader = model.loader;
+                    if (loader != null) {
+                        variant.setRenderer(loader.getRenderer());
+                        Constants.LOG.logDebug("CustomRenderer found: " + variant.getModel().getFormatted());
+                    }
+                });
+    }
+}
